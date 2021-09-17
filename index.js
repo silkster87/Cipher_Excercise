22cc629ae82a378fc5d28af8827613984a4a694b
Silky Ng <silky.ng@gmail.com>
2021-09-17 18:37:34 +0100

    expressed limits in terms of shiftNo

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const { charToInt, intToChar } = require('./helpers');
 
 const N = 26; // Might be useful
 
+const shiftNo = 2; //Number of places to shift
+
 function shiftChar(char){
 
   if(char != ' '){ //No spaces and is a letter
       const charInt = charToInt(char)
-    if(charInt <= 23 && charInt >=0){ 
-      return intToChar(charInt + 2)
-  }else if(charInt >= 24){
-      return intToChar(charInt-24) //Loop back to start charCode
+    if(charInt <= N - 1 - shiftNo && charInt >=0){ 
+      return intToChar(charInt + shiftNo)
+  }else if(charInt >= N - shiftNo){
+      return intToChar(charInt-(N - shiftNo)) //Loop back to start charCode
   } 
   }else if(char == ' '){
     return char
@@ -22,10 +24,10 @@ function unShiftChar(char){
   
   if(char != ' '){
     const charInt = charToInt(char)
-    if(charInt <=25 && charInt >=2){
-      return intToChar(charInt - 2)
-    }else if(charInt <= 1){
-      return intToChar(charInt + 24) //Loop back to end
+    if(charInt <=25 && charInt >=shiftNo){
+      return intToChar(charInt - shiftNo)
+    }else if(charInt <= shiftNo-1){
+      return intToChar(charInt + (N - shiftNo)) //Loop back to end
     }
   }else if(char == ' '){
     return char
