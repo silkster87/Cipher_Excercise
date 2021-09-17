c8ec27ce05fe5dbc2796c3558308368bdcb54688
Silky Ng <silky.ng@gmail.com>
2021-09-17 17:46:13 +0100

    ensure lowercase and tricky message shifted

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const { charToInt, intToChar } = require('./helpers');
 
 const N = 26; // Might be useful
 
-const shiftChar = char => (
-  intToChar(
-    charToInt(char) + 2 // Broken!
-  )
-);
+function shiftChar(char){
+
+  const charLowCase = char.toLowerCase();
+
+  if(charLowCase != " "){ //No spaces and is a letter
+      const charInt = charToInt(charLowCase)
+    if(charInt <= 23 && charInt >=0){ 
+      return intToChar(charInt + 2)
+  }else if(charInt >= 24){
+      return intToChar(charInt-24) //Loop back to start charCode
+  }
+    else{
+      return char
+    }
+  }
+}
 
 const encrypt = message => (
   message
